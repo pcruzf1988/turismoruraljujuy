137d596f41c7c2e7c9ab28263d6dcce3b8828095
pcruzf1988 <pcruzf1988@gmail.com>
2026-01-15 22:20:06 -0300

    se agrego boton de cómo llegar vinculado a google maps

--- js/script.js
+++ js/script.js
@@ -647,6 +647,10 @@ function abrirModal(emprendimiento) {
     const comunidad = emprendimiento['Comunidad / Pueblo'];
     const infoAtencion = emprendimiento['Info / Atención / Condiciones de reserva'];
     
+    // Obtener coordenadas para Google Maps
+    const ubicacion = emprendimiento['Ubicación (formato: -23.5772, -65.3969 latitud,longitud)'];
+    const coords = parsearCoordenadas(ubicacion);
+    
     // Recopilar todas las imágenes disponibles
     const imagenes = [
         emprendimiento.Imagen,
@@ -768,6 +772,15 @@ function abrirModal(emprendimiento) {
                             <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/>
                         </svg>
                         <span>Sitio Web</span>
+                    </a>
+                ` : ''}
+                
+                ${coords ? `
+                    <a href="https://www.google.com/maps/search/?api=1&query=${coords[0]},${coords[1]}" target="_blank" class="modal__contact-btn modal__contact-btn--maps" title="Cómo llegar">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/>
+                        </svg>
+                        <span>Cómo llegar</span>
                     </a>
                 ` : ''}
             </div>
